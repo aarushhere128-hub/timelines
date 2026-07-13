561258b7dda840b64dd5e24931085312c3024d01
aarushhere128-hub <aarushhere128@gmail.com>
2026-07-13 16:34:48 +0530

    Create terminal.js

---
+++ js/terminal.js
@@ -0,0 +1,140 @@
+// ======================================
+// TIMELINES: THE ARCHIVE
+// terminal.js
+// ======================================
+
+
+const screen = document.querySelector(".screen");
+
+
+// --------------------------------------
+// ARGUS Typewriter
+// --------------------------------------
+
+export function typeLine(text, speed = 30){
+
+    return new Promise(resolve => {
+
+        const p = document.createElement("p");
+
+        screen.appendChild(p);
+
+        let i = 0;
+
+
+        const interval = setInterval(()=>{
+
+            p.textContent += text.charAt(i);
+
+            i++;
+
+
+            if(i >= text.length){
+
+                clearInterval(interval);
+                resolve();
+
+            }
+
+
+        }, speed);
+
+    });
+
+}
+
+
+
+// --------------------------------------
+// Terminal Input
+// --------------------------------------
+
+export function ask(question){
+
+    return new Promise(resolve=>{
+
+
+        const line = document.createElement("p");
+
+        line.textContent = question;
+
+        screen.appendChild(line);
+
+
+
+        const inputLine = document.createElement("p");
+
+
+        const prompt = document.createElement("span");
+
+        prompt.textContent = "> ";
+
+
+
+        const typing = document.createElement("span");
+
+        typing.contentEditable = true;
+
+        typing.className = "terminalInput";
+
+        typing.spellcheck = false;
+
+
+
+        inputLine.appendChild(prompt);
+
+        inputLine.appendChild(typing);
+
+
+        screen.appendChild(inputLine);
+
+
+        typing.focus();
+
+
+
+        typing.addEventListener("keydown",(e)=>{
+
+
+            if(e.key === "Enter"){
+
+
+                e.preventDefault();
+
+
+                const answer = typing.textContent.trim();
+
+
+                if(answer === "") return;
+
+
+
+                typing.contentEditable = false;
+
+
+
+                resolve(answer);
+
+
+            }
+
+
+        });
+
+
+    });
+
+
+}
+
+
+
+// --------------------------------------
+// Clear Screen
+// --------------------------------------
+
+export function clearTerminal(){
+
+    screen.innerHTML = "";
+
+}

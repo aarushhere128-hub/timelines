f669fd57f053f4380e317e7b464c7b7e28a72e9a
aarushhere128-hub <aarushhere128@gmail.com>
2026-07-15 13:31:40 +0530

    Create dialogueEngine.js

---
+++ js/dialogueEngine.js
@@ -0,0 +1,125 @@
+// ======================================
+// TIMELINES: THE ARCHIVE
+// dialogueEngine.js
+// ======================================
+
+
+import {
+
+    typeLine,
+    clearTerminal
+
+} from "./terminal.js";
+
+
+
+let currentDialogue;
+
+
+
+// --------------------------------------
+// Start Dialogue
+// --------------------------------------
+
+export async function startDialogue(dialogue){
+
+
+    currentDialogue = dialogue;
+
+
+    await showDialogueLine(0);
+
+
+}
+
+
+
+// --------------------------------------
+// Show Dialogue Line
+// --------------------------------------
+
+async function showDialogueLine(index){
+
+
+    const line =
+    currentDialogue.lines[index];
+
+
+    if(!line){
+
+        return;
+
+    }
+
+
+    clearTerminal();
+
+
+    await typeLine(
+        `${line.speaker}:`
+    );
+
+
+    await typeLine(
+        line.text
+    );
+
+
+    showChoices(
+        line.choices
+    );
+
+
+}
+
+
+
+// --------------------------------------
+// Show Choices
+// --------------------------------------
+
+function showChoices(choices){
+
+
+    const container =
+    document.getElementById("timelineChoices");
+
+
+    container.innerHTML="";
+
+
+
+    choices.forEach(choice=>{
+
+
+        const button =
+        document.createElement("button");
+
+
+        button.className =
+        "timelineOption";
+
+
+        button.textContent =
+        "> " + choice.text;
+
+
+
+        button.onclick = ()=>{
+
+
+            showDialogueLine(
+                choice.next
+            );
+
+
+        };
+
+
+        container.appendChild(button);
+
+
+    });
+
+
+}

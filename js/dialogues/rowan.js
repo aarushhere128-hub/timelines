517228d19ed8838a95a50d20db0186b828e6ac0a
aarushhere128-hub <aarushhere128@gmail.com>
2026-07-15 13:32:40 +0530

    Create rowan.js

---
+++ js/dialogues/rowan.js
@@ -0,0 +1,115 @@
+// ======================================
+// Elder Rowan Dialogue
+// ======================================
+
+
+export const rowanDialogue = {
+
+
+id:"rowan",
+
+
+lines:[
+
+
+{
+
+speaker:"Rowan",
+
+text:
+
+"You are not from here.",
+
+
+choices:[
+
+{
+
+text:"How did you know?",
+
+next:1
+
+},
+
+{
+
+text:"What happened here?",
+
+next:2
+
+}
+
+]
+
+},
+
+
+
+{
+
+speaker:"Rowan",
+
+text:
+
+"Because you look at this place like someone searching for answers.",
+
+
+choices:[
+
+{
+
+text:"What answers?",
+
+next:3
+
+}
+
+]
+
+},
+
+
+
+{
+
+speaker:"Rowan",
+
+text:
+
+"Everyone asks what happened. Few ask why it happened.",
+
+
+choices:[
+
+{
+
+text:"Explain.",
+
+next:3
+
+}
+
+]
+
+},
+
+
+
+{
+
+speaker:"Rowan",
+
+text:
+
+"Some histories are buried because they are painful. Others because they are dangerous.",
+
+
+choices:[]
+
+}
+
+
+]
+
+
+};
